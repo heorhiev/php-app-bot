a7eff8a65beb81acdf70d3e195b7952e9a4c3f08
dev8 <dev8.sofona@gmail.com>
2023-07-15 15:44:25 +0300

    + bootstrap

---
+++ src/Bootstrap.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace app\bot;
+
+use app\toolkit\components\bootstrap\BootstrapInterface;
+
+
+class Bootstrap implements BootstrapInterface
+{
+    public function bootstrap($app)
+    {
+
+    }
+}
